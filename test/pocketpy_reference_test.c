407dc5db2f8e120c3795fe4074a5e6a90bf5f77a
moluopro <moluopro@qq.com>
2026-07-20 17:43:08 +0800

    build(pocketpy): isolate the stock runtime

---
+++ test/pocketpy_reference_test.c
@@ -0,0 +1,23 @@
+#include <pocketpy/pocketpy.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    py_initialize();
+
+    if(!py_exec("19 + 23", "<unijit-pocketpy-smoke>", EVAL_MODE, NULL)) {
+        py_printexc();
+        py_finalize();
+        return EXIT_FAILURE;
+    }
+    if(!py_isint(py_retval()) || py_toint(py_retval()) != 42) {
+        fputs("stock PocketPy embedding returned the wrong result\n", stderr);
+        py_finalize();
+        return EXIT_FAILURE;
+    }
+
+    py_finalize();
+    puts("stock PocketPy 2.1.8 embedding smoke test passed");
+    return EXIT_SUCCESS;
+}
